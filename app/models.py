41ea6ff3048dff82171fc07c4add7959aa982e31
Anton Koluh <antonkoluh1@gmail.com>
2026-03-24 12:42:53 +0200

    added indexes

--- app/models.py
+++ app/models.py
@@ -1,14 +1,28 @@
-from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy.orm import Mapped, mapped_column
-from .services.database import Base
 from datetime import datetime
 from typing import Any
+
+from sqlalchemy import Index, DateTime
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Telemetry(Base):
     __tablename__ = "telemetry"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    device: Mapped[str]
-    timestamp: Mapped[datetime]
-    payload: Mapped[dict[str, Any]] = mapped_column(JSONB)
+    device: Mapped[str] = mapped_column(nullable=False)
+    timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    payload: Mapped[dict[str, Any]] = mapped_column(JSONB, nullable=False)
+
+    __table_args__ = (
+        Index(
+            "idx_telemetry_device_time",
+            "device",
+            timestamp.desc(),
+        ),
+        Index(
+            "idx_telemetry_payload_gin",
+            "payload",
+            postgresql_using="gin",
+        ),
+    )
