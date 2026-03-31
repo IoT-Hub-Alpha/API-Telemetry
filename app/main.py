c573f86e23fabe3ce5b40bd0e23597954b24d6da
Anton Koluh <antonkoluh1@gmail.com>
2026-03-31 11:32:15 +0300

    workin 0.7

--- app/main.py
+++ app/main.py
@@ -10,10 +10,19 @@ from fastapi_pagination.links import Page
 from fastapi_pagination import add_pagination
 from fastapi_pagination.ext.sqlalchemy import paginate
 from fastapi.middleware.cors import CORSMiddleware
+from iot_logging import FastAPIRequestContextMiddleware, StructuredJsonFormatter
+import logging
 
 app = FastAPI()
 
 add_pagination(app)
+
+logging.basicConfig(level=logging.INFO)
+for handler in logging.root.handlers:
+    handler.setFormatter(StructuredJsonFormatter())
+
+app.add_middleware(FastAPIRequestContextMiddleware)
+
 
 # allow everyone, since its dev, why the hell not, right???
 app.add_middleware(
@@ -46,12 +55,13 @@ async def get_telemetry(device: str | None = None, after: datetime | None = None
             Telemetry.timestamp < before
         )
         
+    logging.info("fetching telemetry...", extra={"device": device, "after": after, "before": before})
     return paginate(qs)
 
 @app.get("v1/telemetry/aggregates")
 async def get_aggregates(request: Request):
     url = os.getenv("SCALA_URL", "http://scala-aggregation-service:8081/agg")
-    
+    logging.info("forwarding request to scala...")
     try:
         async with httpx.AsyncClient(timeout=10.0) as client:
             response = await client.get(url, params=request.query_params)
@@ -59,13 +69,17 @@ async def get_aggregates(request: Request):
             return response.json()
 
     except httpx.ConnectError as exc:
+        logging.info("HTTPException", extra={"error": repr(exec), "code": 502})
         raise HTTPException(status_code=502, detail=f"Connect error: {repr(exc)}")
     except httpx.ReadTimeout as exc:
+        logging.info("ReadTimeout", extra={"error": repr(exec), "code": 502})
         raise HTTPException(status_code=502, detail=f"Timeout: {repr(exc)}")
     except httpx.HTTPStatusError as exc:
+        logging.info("HTTPStatusError", extra={"error": repr(exec), "code": 502})
         raise HTTPException(
             status_code=502,
             detail=f"Scala returned {exc.response.status_code}: {exc.response.text}"
         )
     except httpx.RequestError as exc:
+        logging.info("RequestError", extra={"error": repr(exec), "code": 502})
         raise HTTPException(status_code=502, detail=f"Request error: {repr(exc)}")
